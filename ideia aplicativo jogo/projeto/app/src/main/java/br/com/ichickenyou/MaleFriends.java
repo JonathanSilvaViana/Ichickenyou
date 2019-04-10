c3bf07bee5db86e6f244db250f2e27d30a2cbdf1
Jonathan Silva Viana <jonathantepes@hotmail.com>
2019-04-09 22:17:04 -0300

    09/04/2019

---
+++ ideia aplicativo jogo/projeto/app/src/main/java/br/com/ichickenyou/MaleFriends.java
@@ -0,0 +1,13 @@
+package br.com.ichickenyou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaleFriends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_male_friends);
+    }
+}
