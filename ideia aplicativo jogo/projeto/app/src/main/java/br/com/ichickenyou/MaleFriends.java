1b3cd1df1799e12d28111d0d8a284a710a0cf680
Jonathan Silva Viana <jonathantepes@hotmail.com>
2019-04-29 13:58:11 -0300

    29/04/2019

--- ideia aplicativo jogo/projeto/app/src/main/java/br/com/ichickenyou/MaleFriends.java
+++ ideia aplicativo jogo/projeto/app/src/main/java/br/com/ichickenyou/MaleFriends.java
@@ -1,15 +1,75 @@
 package br.com.ichickenyou;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MaleFriends extends AppCompatActivity {
+
+    ImageButton bt_adicionar;
+    LinearLayout dynamicview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_male_friends);
+
+        bt_adicionar = (ImageButton)findViewById(R.id.bt_adicionar);
+
+        dynamicview = (LinearLayout)findViewById(R.id.conteudo_amigos);
+        LinearLayout.LayoutParams lprams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        bt_adicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                for (int i=0; i<1; i++)
+                {
+                    Button btn = new Button(MaleFriends.this);
+                    btn.setId(i+1);
+                    btn.setText("Button"+(i+1));
+                    //btn.setLayoutParams(lprams);
+                    final int index = i;
+                    //btn.setOnClickListener(chama);
+                    dynamicview.addView(btn);
+                }
+
+
+            }
+        });
+
     }
+
+//    @Override
+//    public void onClick(View view){
+//        switch(view.getId()){
+//            case 1:
+//                //first button click
+//                break;
+//            //Second button click
+//            case 2:
+//                break;
+//            case 3:
+//                //third button click
+//                break;
+//            case 4:
+//                //fourth button click
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 
     public void finish()
     {
