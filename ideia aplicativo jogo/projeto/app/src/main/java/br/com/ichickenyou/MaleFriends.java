546127e7ae411377e8dd7b8b9a6b36062e6cc317
Jonathan Silva Viana <jonathantepes@hotmail.com>
2019-05-07 19:32:39 -0300

    07/05/2019

--- ideia aplicativo jogo/projeto/app/src/main/java/br/com/ichickenyou/MaleFriends.java
+++ ideia aplicativo jogo/projeto/app/src/main/java/br/com/ichickenyou/MaleFriends.java
@@ -24,13 +24,12 @@ import android.widget.Toast;
 
 public class MaleFriends extends AppCompatActivity {
 
-    ImageButton bt_adicionar, bt_retorna;
+    ImageButton bt_adicionar, bt_avanca_resultado;
     LinearLayout dynamicview;
     ScrollView scrollView_content_friends;
     FloatingActionButton bt_scroll2up;
     AutoCompleteTextView nome_autocomplete, fazer_autocomplete;
     ArrayAdapter<String> nomes_masculinos, partes_galinha, gosta_de_fazer_ou_faz;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,10 +68,10 @@ public class MaleFriends extends AppCompatActivity {
         //instancia que localiza o scrollview da activity
         scrollView_content_friends = (ScrollView)findViewById(R.id.scrollView_content_friends);
 
+        //cria a instancia localizando o botão que avança ao resultado
+        bt_avanca_resultado = (ImageButton)findViewById(R.id.bt_next_step);
 
-        bt_retorna = (ImageButton)findViewById(R.id.bt_next_step);
-
-        bt_retorna.setOnClickListener(new View.OnClickListener() {
+        bt_avanca_resultado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MaleFriends.this, "clicado", Toast.LENGTH_SHORT).show();
@@ -112,143 +111,127 @@ public class MaleFriends extends AppCompatActivity {
                 //metodo que desce o scrollview ao final
                 scrollView_content_friends.fullScroll(View.FOCUS_DOWN);
 
-                //cria a textview que anuncia o pedido do nome do amigo(a)
+                //gera o formulário sintético
 
-                for (int txtVw1 = 0; txtVw1 < 1; txtVw1++)
+                for (int form = 0; form < 1; form ++)
                 {
-                    TextView txtVw_escolhanome = new TextView(MaleFriends.this);
-                    txtVw_escolhanome.setId(txtVw1 + 1);
+                    //cria a textview que anuncia o pedido do nome do amigo(a)
+
+                    final TextView txtVw_escolhanome = new TextView(MaleFriends.this);
+                    txtVw_escolhanome.setId(form + 1);
                     txtVw_escolhanome.setText(R.string.titlemenunames);
-                    final int index = txtVw1;
+                    final int index = form;
                     dynamicview.addView(txtVw_escolhanome);
                     Log.i("TextView escolha o nome", "Index: " + index);
-                }
 
-                //cria a autocompletetextview onde o usuário pode escrever o nome do amigo(a)
+                    //cria a autocompletetextview onde o usuário pode escrever o nome do amigo(a)
 
-                for (int nome_autocomplete = 0; nome_autocomplete < 1; nome_autocomplete++)
-                {
-                    AutoCompleteTextView nome_autocomplete_field = new AutoCompleteTextView(MaleFriends.this);
-                    nome_autocomplete_field.setId(nome_autocomplete + 1);
-                    final int index = nome_autocomplete;
+                    final AutoCompleteTextView nome_autocomplete_field = new AutoCompleteTextView(MaleFriends.this);
+                    nome_autocomplete_field.setId(form + 1);
+                    final int index2 = form;
                     nome_autocomplete_field.setAdapter(nomes_masculinos);
                     dynamicview.addView(nome_autocomplete_field);
-                    Log.i("ATxtView escreve o nome", "Index: " + index);
-                }
+                    Log.i("ATxtView escreve o nome", "Index: " + index2);
 
-                //cria a textview que anuncia a seleção da parte da galinha
+                    //cria a textview que anuncia a seleção da parte da galinha
 
-                for (int txtVw2 = 0; txtVw2 < 1; txtVw2++)
-                {
-                    TextView txtVw_escolhaparte = new TextView(MaleFriends.this);
-                    txtVw_escolhaparte.setId(txtVw2 + 1);
+                    final TextView txtVw_escolhaparte = new TextView(MaleFriends.this);
+                    txtVw_escolhaparte.setId(form + 1 + 1);
                     txtVw_escolhaparte.setText(R.string.titlechickenparts);
-                    final int index = txtVw2;
+                    final int index3 = form;
                     dynamicview.addView(txtVw_escolhaparte);
-                    Log.i("TxtV escolha a parte", "Index: " + index);
-                }
+                    Log.i("TxtV escolha a parte", "Index: " + index3);
 
-                //cria o spinner onde o usuário seleciona as partes da galinha
+                    //cria o spinner onde o usuário seleciona as partes da galinha
 
-                for (int spinner = 0; spinner < 1; spinner++)
-                {
-
-                    Spinner galinha_spinner = new Spinner(MaleFriends.this);
+                    final Spinner galinha_spinner = new Spinner(MaleFriends.this);
                     //partes_galinha = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, getResources().getStringArray(R.array.malenames));
-                    galinha_spinner.setId(spinner + 1);
+                    galinha_spinner.setId(form + 1);
                     galinha_spinner.setAdapter(partes_galinha);
-                    final int index = spinner;
+                    final int index4 = form;
                     dynamicview.addView(galinha_spinner);
-                    Log.i("cria spinner da galinha", "Index: " + index);
-                }
+                    Log.i("cria spinner da galinha", "Index: " + index4);
 
-                //cria a textview que anuncia a ações
+                    //cria a textview que anuncia a ações
 
-                for (int txtVw3 = 0; txtVw3 < 1; txtVw3++)
-                {
-                    TextView txtVw_escolhaacao = new TextView(MaleFriends.this);
-                    txtVw_escolhaacao.setId(txtVw3 + 1);
+                    final TextView txtVw_escolhaacao = new TextView(MaleFriends.this);
+                    txtVw_escolhaacao.setId(form + 1 + 1 + 1);
                     txtVw_escolhaacao.setText(R.string.titlethingstodo);
-                    final int index = txtVw3;
+                    final int index5 = form;
                     dynamicview.addView(txtVw_escolhaacao);
-                    Log.i("TxtV escolha a ação", "Index: " + index);
-                }
+                    Log.i("TxtV escolha a ação", "Index: " + index5);
 
-                //cria a autocompletetextview onde o usuário pode escrever o nome da ação
+                    //cria a autocompletetextview onde o usuário pode escrever o nome da ação
 
-                for (int nome_acao = 0; nome_acao < 1; nome_acao++)
-                {
-                    AutoCompleteTextView nome_acao_field = new AutoCompleteTextView(MaleFriends.this);
-                    nome_acao_field.setId(nome_acao + 1);
-                    final int index = nome_acao;
+                    final AutoCompleteTextView nome_acao_field = new AutoCompleteTextView(MaleFriends.this);
+                    nome_acao_field.setId(form + 1 + 1);
+                    final int index6 = form;
                     nome_acao_field.setAdapter(gosta_de_fazer_ou_faz);
                     dynamicview.addView(nome_acao_field);
-                    Log.i("ATxtView escreve a ação", "Index: " + index);
-                }
+                    Log.i("ATxtView escreve a ação", "Index: " + index6);
 
-                //cria o separador claro
-                for (int separador = 0; separador < 1; separador++)
-                {
-                    TextView linha_separador = new TextView(MaleFriends.this);
-                    linha_separador.setId(separador + 1);
+                    //cria o separador claro
+
+                    final TextView linha_separador = new TextView(MaleFriends.this);
+                    linha_separador.setId(form + 1 + 1 + 1 + 1);
                     linha_separador.setBackgroundColor(Color.WHITE);
                     linha_separador.setTextSize(1);
                     linha_separador.setText(" ");
                     dynamicview.addView(linha_separador);
-                }
 
-                //cria o botão excluir form
-                for (int btForm = 0; btForm < 1; btForm++)
-                {
+                    //cria o botão excluir form
+
                     final Button bt_delete_form = new Button(MaleFriends.this);
-                    bt_delete_form.setId(btForm + 1);
+                    bt_delete_form.setId(form + 1);
                     bt_delete_form.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                     bt_delete_form.setTextColor(Color.WHITE);
                     bt_delete_form.setText(R.string.bt_erase_text);
+                    dynamicview.addView(bt_delete_form);
+
+                    //cria o segundo separador claro
+
+                    final TextView linha_separador2 = new TextView(MaleFriends.this);
+                    linha_separador2.setId(form + 1 + 1 + 1 + 1 + 1);
+                    linha_separador2.setBackgroundColor(Color.WHITE);
+                    linha_separador2.setTextSize(1);
+                    linha_separador2.setText(" ");
+                    dynamicview.addView(linha_separador2);
+
+                    //cria o separador escuro
+
+                    final TextView linha_separador3 = new TextView(MaleFriends.this);
+                    linha_separador3.setId(form + 1 + 1 + 1 + 1 + 1 + 1);
+                    linha_separador3.setBackgroundColor(Color.BLACK);
+                    linha_separador3.setTextSize(1);
+                    linha_separador3.setText(" ");
+                    dynamicview.addView(linha_separador3);
+
+                    //quando o botão de deletar form for clicado, o formulário onde é contido será deletado junto a todos elementos nele contidos
                     bt_delete_form.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            //apaga todos os campos do formulário, a partir do campo criado
+                            txtVw_escolhanome.setVisibility(View.GONE);
+                            nome_autocomplete_field.setVisibility(View.GONE);
+                            txtVw_escolhaparte.setVisibility(View.GONE);
+                            nome_autocomplete_field.setVisibility(View.GONE);
+                            txtVw_escolhaparte.setVisibility(View.GONE);
+                            galinha_spinner.setVisibility(View.GONE);
+                            txtVw_escolhaacao.setVisibility(View.GONE);
+                            nome_acao_field.setVisibility(View.GONE);
+                            linha_separador.setVisibility(View.GONE);
                             bt_delete_form.setVisibility(View.GONE);
+                            linha_separador2.setVisibility(View.GONE);
+                            linha_separador3.setVisibility(View.GONE);
                         }
                     });
-                    dynamicview.addView(bt_delete_form);
+
                 }
-
-                //cria o segundo separador claro
-                for (int separador = 0; separador < 1; separador++)
-                {
-                    TextView linha_separador = new TextView(MaleFriends.this);
-                    linha_separador.setId(separador + 1);
-                    linha_separador.setBackgroundColor(Color.WHITE);
-                    linha_separador.setTextSize(1);
-                    linha_separador.setText(" ");
-                    dynamicview.addView(linha_separador);
-                }
-
-                //cria o separador escuro
-                for (int separador = 0; separador < 1; separador++)
-                {
-                    TextView linha_separador = new TextView(MaleFriends.this);
-                    linha_separador.setId(separador + 1);
-                    linha_separador.setBackgroundColor(Color.BLACK);
-                    //linha_separador.setLayoutParams(new LinearLayout.LayoutParams(200, 200));
-                    linha_separador.setTextSize(1);
-                    linha_separador.setText(" ");
-                    dynamicview.addView(linha_separador);
-                }
-
-
-
 
             }
         });
 
-
-
-
     }
-
-
 
     public void finish()
     {
